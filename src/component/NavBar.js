e4ab1304b6c75af1badfd080d10b5ef6b874635d
Won A Young <zeronediary@naver.com>
2023-11-14 09:53:01 +0900

    로그인+로그아웃 기능

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,14 +1,23 @@
 import { Button, Flex } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export function NavBar() {
   const navigate = useNavigate();
+
+  function handleLogout() {
+    // TODO: 로그아웃 후 할 일 추가
+    axios.post("/api/member/logout").then(() => console.log("로그아웃 성공"));
+  }
+
   return (
     <Flex>
       <Button onClick={() => navigate("/")}>home</Button>
       <Button onClick={() => navigate("/write")}>write</Button>
       <Button onClick={() => navigate("/signup")}>signup</Button>
       <Button onClick={() => navigate("/member/list")}>회원목록</Button>
+      <Button onClick={() => navigate("/login")}>로그인</Button>
+      <Button onClick={handleLogout}>로그아웃</Button>
     </Flex>
   );
 }
