9aae0295ff30456295f6bb30930bb2bdc2f23a6e
Won A Young <zeronediary@naver.com>
2023-11-14 15:55:12 +0900

    admin 여부 파악 기능

--- src/App.js
+++ src/App.js
@@ -49,13 +49,26 @@ function App(props) {
   function isAuthenticated() {
     return login !== ""; // 빈 스트링이 아니면 로그인한 상태
   }
+  function isAdmin() {
+    if (login.auth) {
+      // 하나라도 name이 admin인 배열 요소가 있는지
+      return login.auth.some((elem) => elem.name === "admin");
+    }
+
+    return false;
+  }
+
+  // function hasAuth(auth){
+  //   // 하나라도 name이 admin인 배열 요소가 있는지
+  //   return login.auth.some(elem=>elem.name===auth);
+  // }
 
   function hasAccess(userId) {
     return login.id === userId;
   }
   return (
     <LoginContext.Provider
-      value={{ login, fetchLogin, isAuthenticated, hasAccess }}
+      value={{ login, fetchLogin, isAuthenticated, hasAccess, isAdmin }}
     >
       <RouterProvider router={routes} />
     </LoginContext.Provider>
