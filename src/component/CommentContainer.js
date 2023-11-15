3d7732b5a8565a5921cdb6db9bf5d1f180878ae9
Won A Young <zeronediary@naver.com>
2023-11-15 10:36:42 +0900

    댓글 목록

--- src/component/CommentContainer.js
+++ src/component/CommentContainer.js
@@ -1,6 +1,6 @@
 import { Box, Button, Textarea } from "@chakra-ui/react";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function CommentForm({ boardId }) {
   const [comment, setComment] = useState("");
@@ -22,7 +22,17 @@ function CommentForm({ boardId }) {
   );
 }
 
-function CommentList() {
+function CommentList({ boardId }) {
+  const [commentList, setCommentList] = useState(null);
+
+  useEffect(() => {
+    const params = new URLSearchParams();
+    params.set("id", boardId);
+    axios
+      .get("/api/comment/list?" + params) //더하기 연산자 덕분에 toString 없어도 String으로 들어가긴 함
+      .then((response) => setCommentList(response.data));
+  }, []);
+
   return <Box>댓글리스트</Box>;
 }
 
@@ -30,7 +40,7 @@ export function CommentContainer({ boardId }) {
   return (
     <Box>
       <CommentForm boardId={boardId} />
-      <CommentList />
+      <CommentList boardId={boardId} />
     </Box>
   );
 }
