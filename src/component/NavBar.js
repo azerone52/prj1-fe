d2bd6c25e956d8389dbf3642a0be97b82db4d2a0
Won A Young <zeronediary@naver.com>
2023-11-22 11:05:55 +0900

    로그인 한 회원 별명 navbar에 출력

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,4 +1,4 @@
-import { Button, Flex, useToast } from "@chakra-ui/react";
+import { Box, Button, Flex, useToast } from "@chakra-ui/react";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useContext, useEffect } from "react";
